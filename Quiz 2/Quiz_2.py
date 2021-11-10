b18bd73dbecb2a46f7b6a6af3b7edab0e7b5f0b0
Josh Farish <josh_farish1@baylor.edu>
2021-11-10 15:28:03 -0600

    Started quiz 2

---
+++ Quiz 2/Quiz_2.py
@@ -0,0 +1,9 @@
+from sklearn import KNeighborsClassifier
+
+
+#1) load the dataset and use the KNeighborsClassifier to train and test your model
+
+
+#2) Display all wrong predicted and expected pairs
+
+#3) produce a csv file of the name of the game and the predicted rating
